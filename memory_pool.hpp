6c827e21612ffeef05671ab5ab1d48e64730885b
dd87 <dima.dunya@gmail.com>
2022-10-27 11:39:11 +0300

    all ingredients've been prepared

--- memory_pool.hpp
+++ memory_pool.hpp
@@ -2,44 +2,21 @@
 
 #include <cassert>
 #include <cstddef>
-#include <map>
 #include <mutex>
 #include <vector>
 
 #include "swap.hpp"
 
-class BlockAddress {
-    bool inMemory;
-    void *memoryAddress;
-    size_t fileAddress;
-
-  public:
-    BlockAddress();
-    BlockAddress(void *memoryAddress);
-
-    void moveToFile(size_t fileAddress);
-    void moveToMemory();
-    bool isInMemory();
-    bool isInSwap();
-    void *getMemoryAddress();
-    size_t getFileAddress();
-};
-
-struct MemoryBlockState {
-    size_t id;
-    bool locked;
-};
-
 class MemoryPool;
 
 class MemoryBlock {
+    void* ptr;
     size_t id;
-    void *ptr;
     size_t size;
     MemoryPool *pool;
 
   public:
-    MemoryBlock(size_t id, void *ptr, size_t size, MemoryPool *pool);
+    MemoryBlock(void *ptr, size_t id, size_t size, MemoryPool *pool);
 
     template <typename T = char> T *getPtr() {
         lock();
@@ -59,22 +36,14 @@ class MemoryPool {
     size_t totalSize;
     char *memoryPtr;
     char *nextBlock;
-    std::mutex poolMutex;
-    static size_t blocksCounter;
-
-    std::vector<MemoryBlockState> blockState;
-    size_t unlockedBlocksCounter;
-    std::mutex blockStateMutex;
-
-    std::map<size_t, BlockAddress> blockAddress;
-    Swap *swap;
+    std::mutex mutex;
+    DiskSwap *swap;
 
     void *privateAlloc();
     void privateFree(void *ptr, size_t id);
 
     size_t blockIndexByAddress(void *ptr);
-    void lockBlock(void *ptr);
-    void unlockBlock(void *ptr);
+	char* blockAddressByIndex(size_t index);
 
     MemoryPool(const MemoryPool &) = delete;
     MemoryPool &operator=(const MemoryPool &) = delete;
@@ -82,6 +51,9 @@ class MemoryPool {
   public:
     MemoryPool(size_t numBlocks, size_t blockSize);
     ~MemoryPool();
+
+	void lockBlock(void *ptr); 
+	void unlockBlock(void *ptr); 
 
     MemoryBlock getBlock();
 };
