98664af073d0e0b51ccdbd6b225378ec9f8c46ea
dd87 <dima.dunya@gmail.com>
2022-11-02 23:27:54 +0300

    forgot to add this file before

---
+++ memory_manager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include <map>
+#include <memory>
+
+#include "config.hpp"
+#include "memory_pool.hpp"
+
+class MemoryManager {
+	const std::vector<size_t> blockSizes {16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
+	std::map<size_t, std::unique_ptr<MemoryPool>> poolMap;
+
+    MemoryManager(const MemoryManager&) = delete;
+    MemoryManager &operator=(const MemoryManager &) = delete;
+public:
+	MemoryManager(size_t memorySize);
+	MemoryBlock getBlock(size_t size); 
+	size_t maxBlockSize() const;
+};
