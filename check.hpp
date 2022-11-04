edf6a4614c97b1716bb824666433bd5647a2d049
dd87 <dima.dunya@gmail.com>
2022-11-04 17:43:36 +0300

    delete bytewise check util, use bash script (faster)

--- check.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-int checkResult();
-bool compareFiles(const std::string &path1, const std::string &path2);
-bool compareAllFilesInFolders(const std::string &dir1, const std::string &dir2);
