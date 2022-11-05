f333e7f87a45370cd328fe06b44a8774e7c47f22
dd87 <dima.dunya@gmail.com>
2022-11-06 00:06:26 +0300

    + adaptive ram size for tests (1/20 data size)

--- run_tests.sh
+++ run_tests.sh
@@ -4,13 +4,20 @@ else
 	numTests=$1
 fi
 
+folder_size_kb=$(du 'input/' | awk '{print $1}')
+let "folder_size_mb = folder_size_kb / 1024 + 1"
+echo "input folder size: $folder_size_mb Mb" 
+
+let "use_ram_size = folder_size_mb / 20 + 1"
+echo "selected ram size: $use_ram_size Mb" 
+
 mkdir -p logs
 
 echo "Run $numTests tests in a row."
 rm ./logs/*
 for i in $(seq 1 $numTests); do 
 	echo -n "# $i ... "; 
-	./build/memory_manager_test 1 > ./logs/log$i.txt && ./check_result.sh >> ./logs/log$i.txt
+	./build/memory_manager_test $use_ram_size > ./logs/log$i.txt && ./check_result.sh >> ./logs/log$i.txt
 	if [ $? -eq 0 ]; then
 		echo "[+]"
 	else 
