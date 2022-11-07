898c57326f27d385f9f3c91fd2ea2aadf9362cc2
dd87 <dima.dunya@gmail.com>
2022-11-07 13:28:06 +0300

    fix check_result.sh problem with filenames with ' '

--- check_result.sh
+++ check_result.sh
@@ -2,13 +2,13 @@ inputDir="./input"
 outputDir="./output"
 
 error=false
-for file in $(ls $inputDir); do
-	filename=$(basename $file)
+for file in $inputDir/*; do
+	filename=$(basename "$file")
 	inputFile="$inputDir/$filename"
 	outputFile="$outputDir/$filename"
 	echo -n "$filename ... " 
-	if [ -f $outputFile ]; then
-		DIFF=$(diff $inputFile $outputFile)
+	if [ -f "$outputFile" ]; then
+		DIFF=$(diff "$inputFile" "$outputFile")
 		if [ "$DIFF" == "" ]; then
 			echo "[+]" 
 		else
