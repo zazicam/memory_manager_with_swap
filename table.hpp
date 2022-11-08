db841c26d60cb826161eef315f48b7bb77887ae0
dd87 <dima.dunya@gmail.com>
2022-11-08 15:46:15 +0300

    + class for creating tables

---
+++ table.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector> 
+#include <string> 
+#include <sstream> 
+
+//---------------------------------------
+// Convinient class for building tables
+// It truncates content automaticaly to
+// the column width.
+//---------------------------------------
+
+struct HorizontalRule {}; 
+extern const HorizontalRule& hr;
+ 
+class Table { 
+    std::vector<size_t> columnWidth; 
+    std::ostringstream buf; 
+    size_t column; 
+    size_t row; 
+public: 
+    Table(std::vector<size_t>&& columnWidth);
+ 
+    Table(const Table&) = delete; 
+    Table operator=(const Table&) = delete; 
+    Table operator=(Table) = delete; 
+
+	void ProcessStream(std::ostringstream& oss); 
+ 
+    template<typename T> 
+    Table& operator<<(T smth) { 
+        std::ostringstream oss; 
+        oss << smth; 
+		ProcessStream(oss);
+        return *this; 
+    } 
+ 
+    Table& operator<<(HorizontalRule hr); 
+ 
+    friend std::ostream& operator<<(std::ostream& out, const Table& table) { 
+        return out << table.buf.str(); 
+    } 
+}; 
