38ef38a00c8500d737406e89d9f571c5a9ff47ad
dd87 <dima.dunya@gmail.com>
2022-11-08 11:34:12 +0300

    Use CMake instead of Makefile

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 if [ $# -eq 0 ]; then
-	numTests=5
+	numTests=1
 else
 	numTests=$1
 fi
@@ -10,10 +10,15 @@ echo "input folder size: $folder_size_mb Mb"
 
 let "use_ram_size = folder_size_mb / 20 + 1"
 echo "selected ram size: $use_ram_size Mb" 
+echo
 
 mkdir -p logs
 
 echo "Run $numTests tests in a row."
+echo "You can change number of tests using command:"
+echo "   $0 [number of tests]"
+echo
+
 rm ./logs/*
 for i in $(seq 1 $numTests); do 
 	echo -n "# $i ... "; 
